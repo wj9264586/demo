f571c3682f0cf1b18a69a635ffb8e098a883e150
wj9264586 <18855057637@163.com>
2019-07-15 20:36:03 +0800

    models

--- demo_codes/models.py
+++ demo_codes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+    SEX= (
+        (0,"男"),
+        (1,"女")
+    )
+    username = models.CharField(
+        max_length=255
+    )
+    age = models.CharField(
+        max_length=255
+    )
+    sex = models.IntegerField(
+        choices=SEX
+    )
